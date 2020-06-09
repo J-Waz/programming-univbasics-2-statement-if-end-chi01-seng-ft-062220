bc9d016fdc7d40a502017f9ce67fb3aea8e698ae
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-09 20:16:23 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -14,7 +14,8 @@
 
 #Example 2 
 
-puts "You know what year it is??"
+#puts "You know what year it is??"
+
 this_year = 2019
-puts "Hey, it's 2019!" if
-this_year == 2019
+puts "Hey, it's 2019!"
+unless this_year == 2019
