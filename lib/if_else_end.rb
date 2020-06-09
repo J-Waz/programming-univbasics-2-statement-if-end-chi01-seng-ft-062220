e55fcfb020b32f2eb1e834cf430fd53fd675010a
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-09 20:23:06 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -16,7 +16,11 @@
 
 #puts "You know what year it is??"
 
-this_year = Time.now.year
-puts "Hey, it's 2019!"
-unless this_year == 2019
-end
+#this_year = Time.now.year
+#puts "Hey, it's 2019!"
+#unless this_year == 2019
+#end
+
+
+#SOLUTION!
+
